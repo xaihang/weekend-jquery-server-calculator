65cc240f9093aee713f7d53c9fba3da88043564d
Xai Hang <xaihang12@gmail.com>
2023-01-28 11:52:52 -0600

    post of inputs success

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,43 @@
 $(document).ready(onReady);
 
+//  state
+let usersInput = []; 
+
 function onReady() {
-    console.log('in onReady...');
+    console.log('in onReady... JQUERY is kicking');
+ 
+  $('#resultBtn').on('click', getUsersInput)
+}
+
+function getUsersInput() {
+
+    console.log('INSIDE usersInput ...');
+
+    let newInputs = {
+        firstNumberInput: $('#firstNumberInput').val(),
+        secondNumberInput: $('#secondNumberInput').val(),
+    } 
+
+    // empty out the input field for the next entry
+    $('#firstNumberInput').val('');
+    $('#secondNumberInput').val('');
+
+    console.log('newInputs = ', newInputs);
+
+    $.ajax({
+        type: "POST",
+        url: '/inputs',
+        data: newInputs,
+
+      }).then(function(response) {
+          console.log("SUCCESS!!! for newInputs");
+
+          // render new data to dom
+          usersInput.push(newInputs);
+            // render();
+        }
+      );
+
+    // render(); 
+
 }
--- server/server.js
+++ server/server.js
@@ -6,10 +6,29 @@ const PORT = 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
+let usersInput = []
 
+app.post('/inputs', (req, res) => {
+    console.log(req)
+    console.log('/inputs of the POST request', req.body);
+    console.log('firstNumberInput:', req.body.firstNumberInput);
+    console.log('secondNumberInput:', req.body.secondNumberInput);
 
+    // capture the variables from req.body
+    const firstNumberInput = req.body.firstNumberInput;
+    // console.log(req.body.firstNumberInput);
+
+    const secondNumberInput = req.body.secondNumberInput;
+
+    // push new input to the userInput []
+    usersInput.push({firstNumberInput, secondNumberInput});
+    console.log('..ajax POST new input objects:', usersInput);
+
+    // send back a status code of 201
+    res.sendStatus(201);
+})
 
 
 app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
+    console.log('Server is ALIVE at port', PORT);
   });
