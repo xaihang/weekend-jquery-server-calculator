c4846bea3fdf384d10adec7b3dbcaa58fc7cfc4a
Xai Hang <xaihang12@gmail.com>
2023-01-28 16:06:08 -0600

    created GET for history log, append log and result to DOM

--- server/server.js
+++ server/server.js
@@ -10,56 +10,66 @@ app.use(express.static('server/public'));
 //     input1: number,
 //     input2: number,
 //     mathInput: 'string',
-//     output: number,
+//     result: number,
 // }
-let historyLogs = []
 
-// POST to get inputs of firstNumber and secondNumber
-app.post('/inputs', (req, res) => {
-    console.log('/inputs of the POST request', req.body);
-    // console.log('firstNumberInput:', req.body.firstNumberInput);
-    // console.log('secondNumberInput:', req.body.secondNumberInput);
-    // console.log('mathInput', req.body.mathInput)
-    
-    // capture the variables from req.body
-    const firstNumberInput = Number(req.body.firstNumberInput);
-    const secondNumberInput = Number(req.body.secondNumberInput);
-    const mathInput = req.body.mathInput; 
-    let result = 0;
+let historyLogs = [];
+let calculatedResult = undefined;
 
-    // push new input to the userInput []
-    historyLogs.push({firstNumberInput, secondNumberInput, mathInput});
-    console.log('..ajax POST new input objects:', historyLogs);
+// POST from client to server side responding to new created inputs
+app.post('/calculated-result', (req, res) => {
+  console.log('/calculated-result of the POST request', req.body);
+  // console.log('firstNumberInput:', req.body.firstNumberInput);
+  // console.log('secondNumberInput:', req.body.secondNumberInput);
+  // console.log('mathInput', req.body.mathInput)
 
-    // logic for mathInputs 
-    switch (mathInput) {
-        case "-":
-            result = firstNumberInput - secondNumberInput 
-            break;
-        case "*":
-            result = firstNumberInput * secondNumberInput
-            break;
-        case "/":
-            result = firstNumberInput / secondNumberInput
-            break;
-        default:
-            result = firstNumberInput + secondNumberInput
-            break;
-    }
+  // capture the variables from req.body
+  let firstNumberInput = Number(req.body.firstNumberInput);
+  let secondNumberInput = Number(req.body.secondNumberInput);
+  let mathInput = req.body.mathInput;
 
-    console.log('the RESULTS =', result);
+  // logic for mathInputs
+  switch (mathInput) {
+    case '-':
+        calculatedResult = firstNumberInput - secondNumberInput;
+      break;
+    case '*':
+        calculatedResult = firstNumberInput * secondNumberInput;
+      break;
+    case '/':
+        calculatedResult = firstNumberInput / secondNumberInput;
+      break;
+    default:
+        calculatedResult = firstNumberInput + secondNumberInput;
+      break;
+  }
 
-    // send back a status code of 201
-    res.sendStatus(201);
-})
+  console.log('the calculatedResult =', calculatedResult);
 
-//  GET req setup
-app.get('/inputs', (req, res) => {
+  // store the data
+  historyLogs.push({ firstNumberInput, secondNumberInput, mathInput, calculatedResult });
+  console.log('..ajax POST from server-side new input objects:', historyLogs);
+
+  // send back a status code of 201
+  res.sendStatus(201);
+});
+
+//  GET req for calculated result
+app.get('/calculated-result', (req, res) => {
+  console.log('calculatedResult: ', calculatedResult);
+  // calculatedResult is a number, number cannot go through UNLESS it is in an object format
+  // put the calculatedResult in curly bracket, send in JSON and standardize with 'what' = result
+  res.send({result: calculatedResult});
+});
+
+//  GET req for history logs
+app.get('/history-logs', (req, res) => {
     console.log('historyLogs: ', historyLogs);
     res.send(historyLogs);
 });
+  
 
-
+// listen PORT 
 app.listen(PORT, () => {
-    console.log('Server is ALIVE at port', PORT);
-  });
+  console.log('Server is ALIVE at port', PORT);
+});
